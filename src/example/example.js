f7cc4d796e213ecdc5c68611e7a1d2d47cadb842
whosesmile <lovemoon@yeah.net>
2017-01-20 11:08:47 +0800

    optmize page & widget animate, compact with react-addons-css-transition-group

--- src/example/example.js
+++ src/example/example.js
@@ -1,5 +1,5 @@
 // 显隐列表
-$(document).on('click', '.ex-group .list:first-child', function () {
+$(document).on('click', '.ex-group .list:first-child', function() {
   var selected = $(this).closest('.ex-group').toggleClass('active');
   $('.ex-group').not(selected).removeClass('active');
 });
@@ -13,7 +13,7 @@ function Router() {
 
 Router.prototype = {
 
-  handler: function () {
+  handler: function() {
     var state = history.state || {};
     // 新增 或 前进
     if (typeof state.index === 'undefined' || state.index > this.routes.length) {
@@ -36,20 +36,24 @@ Router.prototype = {
     }
   },
 
-  buildView: function (name) {
+  buildView: function(name) {
     var page = $('#exui_' + name)
     if (page.length) {
       var view = $(page.html()).appendTo('body');
-      view.addClass('ex-view-in').on('animationend webkitAnimationEnd', function () {
-        view.removeClass('ex-view-in');
+      view.addClass('ex-view-enter');
+      view[0].offsetHeight;
+      view.addClass('ex-view-enter-active').on('transitionend webkitTransitionEnd', function() {
+        view.removeClass('ex-view-enter ex-view-enter-active');
       });
       return view;
     }
   },
 
-  destroyView: function (route) {
+  destroyView: function(route) {
     if (route && route.view) {
-      route.view.addClass('ex-view-out').on('animationend webkitAnimationEnd', function () {
+      route.view.addClass('ex-view-leave');
+      route.view[0].offsetHeight;
+      route.view.addClass('ex-view-leave-active').on('transitionend webkitTransitionEnd', function() {
         $(this).remove();
       });
     }
@@ -64,85 +68,60 @@ var router = new Router();
 // WidgetManager
 // 工具类，浮层切换动画
 function WidgetManager() {
-  this.wrapper = null;
   this.widgets = [];
 }
 
 WidgetManager.prototype = {
 
-  // 私有
-  _dismiss: function (e) {
-    if ($(e.target).is('.ex-widget-layer')) {
-      e.data && e.data.hide();
-    }
-  },
-
   // 填充
-  fill: function (templ, opts) {
-    var self = this;
-    if (!this.wrapper) {
-      this.wrapper = $('<div class="ex-widget-layer" />').appendTo('body');
-      this.wrapper.on('click', '[data-dismiss="true"]', function () {
-        self.hide();
-      });
-    }
-
-    opts = opts || {};
-
-    // 重置背景
-    this.wrapper.attr('class', 'ex-widget-layer').toggleClass(opts.clazz, opts.clazz);
-
-    // 点击空白
-    this.wrapper.off('click', this._dismiss);
-    if (opts.dismiss === true) {
-      this.wrapper.on('click', self, this._dismiss);
-    }
-
+  fill: function(templ, opts) {
     // 移除上个组件
     this.popup();
+
+    opts = opts || {};
+    var widget = $('<div class="ex-widget-layer" />').addClass(opts.clazz || '').append(templ);
+
+    // 点击关闭
+    widget.on('click', function(e) {
+      if (e.target.dataset.dismiss === 'true' || (opts.dismiss && $(e.target).is('.ex-widget-layer'))) {
+        this.hide();
+      }
+    }.bind(this));
+
     // 构建当前组件
-    var widget = $(templ).addClass('ex-widget-in');
     this.widgets.push(widget);
+    widget.addClass('ex-widget-enter').appendTo('body');
     // 故意为之 不然不会触发animation
-    this.wrapper[0].offsetHeight;
-    this.wrapper.append(widget).addClass('ex-widget-in');
+    widget[0].offsetHeight;
+    widget.addClass('ex-widget-enter-active');
     return widget;
   },
 
   // 显示
-  show: function (templ, opts, fn) {
-    if (templ) {
-      this.fill(templ, opts).on('animationend webkitAnimationEnd', function () {
-        fn && fn();
-      });
-    }
-  },
-
-  // 隐藏
-  hide: function (fn) {
-    var self = this;
-    if (!self.wrapper) {
-      return false;
-    }
-    self.wrapper.removeClass('ex-widget-in').addClass('ex-widget-out').on('animationend webkitAnimationEnd transitionend webkitTransitionEnd', function () {
-      // 由于样式表不同，可能触发两次事件
-      // 一次Animate,一次transistion 要看背景Layer是否做透明渐变 因此这里做个兼容处理
-      self.wrapper && self.wrapper.remove();
-      self.wrapper = null;
-      self.widget = [];
+  show: function(templ, opts, fn) {
+    var widget = this.fill(templ, opts);
+    widget.on('transitionend webkitTransitionEnd', function() {
+      widget.removeClass('ex-widget-enter ex-widget-enter-active');
       fn && fn();
     });
   },
 
+  // 隐藏
+  hide: function(fn) {
+    this.popup(fn);
+  },
+
   // 交替 (永远只保留一个组件)
-  popup: function () {
+  popup: function(fn) {
     var widget = this.widgets.pop();
     if (!widget) {
       return false;
     }
-    var self = this;
-    widget.removeClass('ex-widget-in').addClass('ex-widget-out').on('animationend webkitAnimationEnd', function () {
+    widget.addClass('ex-widget-leave');
+    widget[0].offsetHeight;
+    widget.addClass('ex-widget-leave-active').on('transitionend webkitTransitionEnd', function() {
       widget.remove();
+      fn && fn();
     });
   },
 
@@ -153,14 +132,13 @@ var widgetManager = new WidgetManager();
 // // // // // // // // // // // // // // // // // // // // // // // // // //
 
 // fastclick 改写自weui,现在支持事件代理和多事件绑定
-(function () {
+(function() {
   // 是否支持touch
-  var supported = (function () {
+  var supported = (function() {
     try {
       document.createEvent("TouchEvent");
       return true;
-    }
-    catch (e) {
+    } catch (e) {
       return false;
     }
   })();
@@ -169,12 +147,12 @@ var widgetManager = new WidgetManager();
   var _onfn = $.fn.on;
 
   // 截取arguments
-  var slice = function (o, i, j) {
+  var slice = function(o, i, j) {
     return Array.prototype.slice.call(o, i, j || o.length);
   };
 
   // 代理改写
-  $.fn.on = function () {
+  $.fn.on = function() {
     var args = arguments;
     var callback = args[args.length - 1];
 
@@ -183,7 +161,7 @@ var widgetManager = new WidgetManager();
       var startY = 0;
 
       // 插入除回调外的初始参数
-      var insertArgs = function (ev, fn) {
+      var insertArgs = function(ev, fn) {
         return [ev].concat(slice(args, 1, args.length - 1)).concat(fn);
       };
 
@@ -220,7 +198,7 @@ var widgetManager = new WidgetManager();
 
   /** 注意: off click 偷懒处理 会误伤touchstart和touchend **/
   var _offfn = $.fn.off;
-  $.fn.off = function () {
+  $.fn.off = function() {
     var args = arguments;
 
     // 包含CLICK && 支持TOUCH && 注册了CALLBACK
@@ -233,8 +211,7 @@ var widgetManager = new WidgetManager();
       if (events) {
         _offfn.apply(this, [events].concat(slice(args, 1)));
       }
-    }
-    else {
+    } else {
       _offfn.apply(this, args);
     }
 
